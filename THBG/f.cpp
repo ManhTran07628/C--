a5b2951b85b7a8dd7c899db4525b99664b962c86
ManhTran07628 <tranducmanh8866@gmail.com>
2026-08-15 00:40:35 +0700

    day code

---
+++ THBG/f.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define oo (ll) 1e18
+const int MM = 1e6 + 7;
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #define taskname ""
+    if(fopen(taskname ".inp","r")) {
+        freopen(taskname ".inp","r",stdin);
+        freopen(taskname ".out","w",stdout);
+    }
+    cout << 36;
+    return 0;
+}
